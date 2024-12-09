f4730f2dc412505bc968bdf5207bfd7def3492a1
zhuziwei <ziwei.zhu777@gmail.com>
2024-12-09 11:31:17 +0800

    KoenigEditor 添加注释

--- packages/koenig-lexical/src/components/KoenigEditor.jsx
+++ packages/koenig-lexical/src/components/KoenigEditor.jsx
@@ -1,3 +1,9 @@
+/*
+ * @Author: zhuziwei
+ * @Date: 2024-12-09 10:54:30
+ * @Last Modified by: zhuziwei
+ * @Last Modified time: Do not edit
+ */
 import '../styles/index.css';
 import KoenigComposableEditor from './KoenigComposableEditor';
 import React from 'react';
@@ -5,6 +11,7 @@ import {AllDefaultPlugins} from '../plugins/AllDefaultPlugins';
 import {SharedHistoryContext} from '../context/SharedHistoryContext';
 import {SharedOnChangeContext} from '../context/SharedOnChangeContext';
 
+// 这个组件是用来创建一个可编辑的文本区域，并添加一些默认的插件。
 const KoenigEditor = ({
     onChange,
     children,
